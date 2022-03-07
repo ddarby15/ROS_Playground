1ef4a350c6836aa1405328bfeeb1feccf9ffd36c
David Darby <dbm.darby@gmail.com>
2022-03-07 11:47:38 -1000

    delete build and devel folders

--- build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ddarby/catkin_ws/src
-# Build directory: /home/ddarby/catkin_ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("autoturtle")
-subdirs("beginner_tutorials")
